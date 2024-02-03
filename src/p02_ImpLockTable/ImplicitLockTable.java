8ed5a4748c0528c0b60b3af234238f88852651de
Roger Esteve <rogeresteve30@gmail.com>
2024-02-03 16:05:38 +0100

    p02: mas o menos

--- src/p02_ImpLockTable/ImplicitLockTable.java
+++ src/p02_ImpLockTable/ImplicitLockTable.java
@@ -5,44 +5,60 @@ import p00_CommonA.Table;
 public class ImplicitLockTable extends Table{
 	
 	/* Declare and initialize tour simple-typed variables here */
+	private boolean permission = true;
 
-	protected void gainExclusiveAccess() {
+	protected synchronized void gainExclusiveAccess() {
 		/* COMPLETE */
+		while (!this.permission || this.ffs >= 4) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		this.permission = false;
 	}
 
 
-	protected void releaseExclusiveAccess() {
+	protected synchronized void releaseExclusiveAccess() {
 		/* COMPLETE */
+		this.permission = true;
 	}
 
 
 	public void putJack(int id) {
 		/* COMPLETE */
+		this.gainExclusiveAccess();
 	}
 
 	
 	public void putQueen(int id) {
 		/* COMPLETE */
+		this.gainExclusiveAccess();
 	}
 
 	
 	public void putKing(int id) {
 		/* COMPLETE */
+		this.gainExclusiveAccess();
 	}
 
 
 	public void cardPut() {
 		/* COMPLETE */
+		this.releaseExclusiveAccess();
 	}
 
 	
 	public void startCheck(int id) {
 		/* COMPLETE */
+		while (this.ffs < 4) {}
 	}
 
 	
 	public void endCheck(int id) {
 		/* COMPLETE */
+		this.ffs = 0;
 	}
 
 }
