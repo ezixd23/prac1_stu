f3ddcfd24d9bec136c10b87d6600699cbe3e5fd4
zelkarrabi <ziad.elkarrabi@gmail.com>
2024-02-08 19:22:46 +0100

    sale el 1 al final pero no el 0 del principio

--- src/p01_MutexTable/MutexTable.java
+++ src/p01_MutexTable/MutexTable.java
@@ -8,43 +8,70 @@ public class MutexTable extends Table{
 
 	
 	/* Declare and initialize your semaphore here */
+	private Semaphore mutex = new Semaphore(1);
 	
 	protected void gainExclusiveAccess () {
 		/* COMPLETE */
+		try {
+			mutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	protected void releaseExclusiveAccess() {
 		/* COMPLETE */
+		mutex.release();
 	}
 	
 	
 	public void putJack(int id) {
 		/* COMPLETE */
+		while(true) {
+			this.gainExclusiveAccess();
+			if((id==0 && this.ffs==0)|| (id==1 && this.ffs==3) || this.ffs < 3) {
+				break;
+			}else this.releaseExclusiveAccess();
+		}
 	}
 	
 	public void putQueen(int id) {
 		/* COMPLETE */
-		
+		while(true) {
+			this.gainExclusiveAccess();
+			if((id==0 && this.ffs==0)|| (id==1 && this.ffs==3) || this.ffs < 3) {
+				break;
+			}else this.releaseExclusiveAccess();
+		}
 	}
 	
 	public void putKing(int id) {
 		/* COMPLETE */
-		
+		while(true) {
+			this.gainExclusiveAccess();
+			if((id==0 && this.ffs==0)||(id==1 && this.ffs==3) || this.ffs < 3) {
+				break;
+			}else this.releaseExclusiveAccess();
+		}
 	}
 
 	
 	public void startCheck(int id) {
 		/* COMPLETE */
+		while (this.ffs < 4) {}
 	}
 
 	
 	public void endCheck(int id) {
 		/* COMPLETE */
+		this.ffs = 0;
 	}
 
 	
 	public void cardPut() {
 		/* COMPLETE */
+		this.releaseExclusiveAccess();
 	}
 
 }
